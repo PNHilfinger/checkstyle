d1d19d7f9f32d298736a56ab4c2db2126894fe4a
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2022-01-11 14:19:32 -0800

    Fix package declaration.  Use -g by default for alt-compile.

--- src/main/java/ucb/checkstyle/checks/JavadocMethod61bCheck.java
+++ src/main/java/ucb/checkstyle/checks/JavadocMethod61bCheck.java
@@ -18,7 +18,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
 
-package ucb.checkstyle;
+package ucb.checkstyle.checks;
 
 import com.puppycrawl.tools.checkstyle.FileStatefulCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
